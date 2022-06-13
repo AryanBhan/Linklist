c9e76310fbdeabeff13f17e426030f6d70715dd3
Aryan <theredsword70@gmail.com>
2022-06-13 11:29:10 +0530

    insertatany

---
+++ insertateny.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+};
+node* head=NULL,*temp;
+void push(int d)
+{
+    node* newnode=new node();
+    newnode->data=d;
+    newnode->next=NULL;
+    if(head==NULL)
+    {
+        head=temp=newnode;
+    }
+    else
+    {
+        temp->next=newnode;
+        temp=newnode;
+    }
+}
+void print()
+{
+    node* ptr=head;
+    while (ptr!=0)
+    {
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+    
+}
+void insertatany(int d,int pos)
+{
+    node * ptr=head,*p;
+    node* newnode=new node();
+    newnode->data=d;
+    pos--;
+    while (pos--)
+    {
+        p=ptr;
+        ptr=ptr->next;
+    }
+    p->next=newnode;
+    newnode->next=ptr;
+
+    
+}
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    insertatany(8,3);
+    print();
+return 0;
+}
